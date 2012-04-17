6cbc49cab144e94926d64be603a48d879ff4a604
Jean-Max Marie-Sainte <j.marie-sainte@novediagroup.com>
2012-04-17 09:01:51 +0000

    Ajout du projet TalentMap-Services au répertoire projet sous SVN.

---
+++ TalentMap-Services/src/main/java/com/novedia/talentmap/services/ITalentMapService.java
@@ -0,0 +1,14 @@
+package com.novedia.talentmap.services;
+
+
+/**
+ * Talent Map business interface service.
+ */
+public interface ITalentMapService {
+	
+    /**
+     * Display Skills
+     * @return the String value of skills
+     */
+    public String displaySkills();
+}
---
+++ TalentMap-Services/src/main/java/com/novedia/talentmap/services/impl/TalentMapService.java
@@ -0,0 +1,55 @@
+package com.novedia.talentmap.services.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.novedia.talentmap.model.dao.ISkillDao;
+import com.novedia.talentmap.model.entity.SkillEntity;
+import com.novedia.talentmap.services.ITalentMapService;
+
+/**
+ * ITalentMapService interface implementation.
+ * 
+ * @author j.marie-sainte
+ */
+public class TalentMapService implements ITalentMapService {
+	
+	/**
+	 * The skill DAO bean
+	 */
+	private ISkillDao skillDao;
+	
+	@Override
+	public String displaySkills() {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		Collection<SkillEntity> skills= skillDao.getSkills();
+//		SkillEntity skillOne = new SkillEntity();
+//		skillOne.setDomain("Java");
+//		skillOne.setLevel(5);
+//		skillOne.setType(2);
+		
+//		Collection<SkillEntity> skills = new ArrayList<SkillEntity>();
+//		skills.add(skillOne);
+		
+		for (SkillEntity skill : skills) {
+			sb.append(skill.getDomain()).append(" / ").append(skill.getLevel()).append(" / ").append(skill.getType());
+		}
+		
+		String message = "Talent Map user skill are : [ ".concat(sb.toString()).concat(" ]");
+		
+		return message;
+	}
+
+	// Setters
+	
+	/**
+	 * Set the skill DAO value
+	 * @param skillDao
+	 */
+	public void setSkillDao(ISkillDao skillDao) {
+		this.skillDao = skillDao;
+	}	
+	
+}
