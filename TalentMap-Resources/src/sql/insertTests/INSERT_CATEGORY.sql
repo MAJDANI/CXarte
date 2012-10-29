14f043ce862ecefaf05e7a1efa1b7a054eff4318
Jean-Max Marie-Sainte <j.marie-sainte@novediagroup.com>
2012-10-29 13:48:20 +0000

    [JMS][TMP][2.0] Mise à jour des scripts de base de données.

--- TalentMap-Resources/src/sql/insertTests/INSERT_CATEGORY.sql
+++ TalentMap-Resources/src/sql/insertTests/INSERT_CATEGORY.sql
@@ -1,4 +1,5 @@
 BEGIN
-  insert into CATEGORY (CATEGORY_ID, CATEGORY_NAME) values (1, 'JAVA');
+  insert into CATEGORY (CATEGORY_ID, CATEGORY_NAME) values (1, 'Java EE');
   insert into CATEGORY (CATEGORY_ID, CATEGORY_NAME) values (2, '.NET');
+  insert into CATEGORY (CATEGORY_ID, CATEGORY_NAME) values (2, 'Php');
 END;
--- TalentMap-Resources/src/sql/insertTests/INSERT_TOOL.sql
+++ TalentMap-Resources/src/sql/insertTests/INSERT_TOOL.sql
@@ -1,5 +1,5 @@
 BEGIN
   insert into TOOL (TOOL_ID, CONCEPT_ID, TOOL_NAME) values (1, 1, 'Spring');
   insert into TOOL (TOOL_ID, CONCEPT_ID, TOOL_NAME) values (2, 2, 'Hibernate');
-  insert into TOOL (TOOL_ID, CONCEPT_ID, TOOL_NAME) values (3, 3, 'Castes Windsor');
+  insert into TOOL (TOOL_ID, CONCEPT_ID, TOOL_NAME) values (3, 3, 'Castel Windsor');
 END;
