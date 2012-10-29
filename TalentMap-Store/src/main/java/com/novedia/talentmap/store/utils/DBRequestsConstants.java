72c661ea0536ce96ac1f96bc620f3e149522e645
Veronique Dibi <v.dibi@novediagroup.com>
2012-10-29 14:08:58 +0000

    [VDI][2.0] création fichier de constante pour les requêtes IBATIS

---
+++ TalentMap-Store/src/main/java/com/novedia/talentmap/store/utils/DBRequestsConstants.java
@@ -0,0 +1,19 @@
+package com.novedia.talentmap.store.utils;
+/**
+ * This class contains static constants representing the request used to fetch TMP datas.
+ * @author j.marie-sainte
+ *
+ */
+public final class DBRequestsConstants {
+	
+	/**
+	* Request used to get a category in TMP data base model.
+	*/
+	public final static String GET_CATEGORY_REQUEST = "category.getCategory";
+
+	/**
+	* Request used to get all category in TMP data base model.
+	*/
+	public final static String GET_ALL_CATEGORY_REQUEST = "category.getAllCategory";
+
+}
