e56a39b5a7382cd799d46529bd0fdab83ecc9dc3
Veronique Dibi <v.dibi@novediagroup.com>
2012-11-08 17:57:25 +0000

    [VDI][2.0] creation fichier sql

---
+++ TalentMap-Resources/src/sql/datas/INIT_CONCEPT.sql
@@ -0,0 +1,18 @@
+
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (1,1,'IOC');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (2,1,'ORM');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (3,2,'IOC');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (4,2,'ORM');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (5,3,'ORM');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (6,3,'IOC');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (7,4,'IOC');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (8,4,'ORM');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (9,5,'SGBD');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (10,1,'Tests Unitaires');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (11,2,'Tests Unitaires');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (12,6,'Conception');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (13,7,'Qualité');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (14,8,'Suivi');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (15,8,'Gestion');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (16,9,'Suivi');	
+insert into CONCEPT (CONCEPT_ID, CATEGORY_ID, CONCEPT_NAME)	values (17,9,'Gestion');	
---
+++ TalentMap-Resources/src/sql/datas/INIT_PROFILE.sql
@@ -0,0 +1,12 @@
+insert into PROFILE (PROFILE_ID, PROFILE_TYPE)
+values (1,'Développeur');
+insert into PROFILE (PROFILE_ID, PROFILE_TYPE)
+values (2,'Fonctionnel');
+insert into PROFILE (PROFILE_ID, PROFILE_TYPE)
+values (3,'Chef de Projet');
+insert into PROFILE (PROFILE_ID, PROFILE_TYPE)
+values (4,'Architecte Technique');
+insert into PROFILE (PROFILE_ID, PROFILE_TYPE)
+values (5,'Architecte Fonct.');
+insert into PROFILE (PROFILE_ID, PROFILE_TYPE)
+values (6,'Testeur');
