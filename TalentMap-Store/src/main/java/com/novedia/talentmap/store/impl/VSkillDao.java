b7e0a44e4c9fad6338d6dbd7ddabb7d1a8fe3ab0
Jean-Max Marie-Sainte <j.marie-sainte@novediagroup.com>
2012-11-09 15:52:42 +0000

    [JMS][2.0] Refactoring implémentation conformément aux modifications d'interface.

--- TalentMap-Store/src/main/java/com/novedia/talentmap/store/impl/VSkillDao.java
+++ TalentMap-Store/src/main/java/com/novedia/talentmap/store/impl/VSkillDao.java
@@ -4,15 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.novedia.talentmap.model.entity.VSkill;
 import com.novedia.talentmap.store.IVSkillDao;
+import com.novedia.talentmap.store.utils.DBRequestsConstants;
 
 /**
+ * The VSkill Data Access Object. 
  * 
  * @author j.collet
  * @project TalentMap-Store
@@ -25,33 +26,28 @@ public class VSkillDao extends SqlMapClientDaoSupport implements IVSkillDao{
 	 * Class builder based on sqlMapClient
 	 * @param sqlMapClient
 	 */
-	@Autowired
 	public VSkillDao(final SqlMapClient sqlMapClient){
 		setSqlMapClient(sqlMapClient);
 	}
 	
-	
 	@SuppressWarnings("unchecked")
 	public List<VSkill> getToolByConcept(String categoryName, String conceptName) throws DataAccessException {
-		
+		// TODO : A REVOIR un tableau de String devrait suffire
 		Map<String, String> mapName = new HashMap<String, String>();
 		mapName.put("categoryName", categoryName);
 		mapName.put("conceptName", conceptName);
-		//return this.getSqlMapClientTemplate().queryForList(DBRequestsConstants.GET_TOOL_BY_CONCEPT, mapName);
-		return this.getSqlMapClientTemplate().queryForList("", mapName);
+		return this.getSqlMapClientTemplate().queryForList(DBRequestsConstants.GET_TOOL_BY_CONCEPT, mapName);
 	}
 	
-	
-	
+	@Override
 	public VSkill getSkillByTool(String toolName) throws DataAccessException {
-		//return (VSkill) this.getSqlMapClientTemplate().queryForObject(DBRequestsConstants.GET_SKILL_BY_TOOL,toolName);
-		return (VSkill) this.getSqlMapClientTemplate().queryForObject("",toolName);
+		return (VSkill) this.getSqlMapClientTemplate().queryForObject(DBRequestsConstants.GET_SKILL_BY_TOOL,toolName);
 	}
 	
 	
-	
+	@SuppressWarnings("unchecked")
+	@Override
 	public List<VSkill> getConceptByCategory(String categoryName) throws DataAccessException {
-		//return this.getSqlMapClientTemplate().queryForList(DBRequestsConstants.GET_CONCEPT_BY_CATEGORY, categoryName);
-		return this.getSqlMapClientTemplate().queryForList("", categoryName);
+		return this.getSqlMapClientTemplate().queryForList(DBRequestsConstants.GET_CONCEPT_BY_CATEGORY, categoryName);
 	}
 }
