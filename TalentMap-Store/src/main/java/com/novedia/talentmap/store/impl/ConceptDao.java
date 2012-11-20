4e9ce79289cf8b029d0fc0c4fb7d09f5dcf43cb2
Elise Moumbe <e.moumbe@novediagroup.com>
2012-11-20 14:16:42 +0000

    EMO: Add logs

--- TalentMap-Store/src/main/java/com/novedia/talentmap/store/impl/ConceptDao.java
+++ TalentMap-Store/src/main/java/com/novedia/talentmap/store/impl/ConceptDao.java
@@ -2,6 +2,8 @@ package com.novedia.talentmap.store.impl;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
 
@@ -20,20 +22,28 @@ import com.novedia.talentmap.store.utils.DBRequestsConstants;
 public class ConceptDao extends SqlMapClientDaoSupport implements IDao<Concept> {
 	
 	
+	/** For log management */
+	private static Log logger = LogFactory.getLog(ConceptDao.class);
+	
 	/**
 	 * Set the sqlMapClient value
 	 * @param sqlMapClient the sqlMapClient to set
 	 */
 	public ConceptDao (final SqlMapClient sqlMapClient){
+		if(logger.isDebugEnabled()) {
+			logger.debug("Create ConceptDao");
+		}
 		setSqlMapClient(sqlMapClient);
 	}
-	
 	
 	/**
 	 * Get One Concept By Id
 	 */
 	@Override
 	public Concept get(Integer id) throws DataAccessException {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Get Concept with id : " + id);
+		}
 		return (Concept) this.getSqlMapClientTemplate().queryForObject(DBRequestsConstants.GET_CONCEPT, id);
 	}
 	
@@ -43,32 +53,64 @@ public class ConceptDao extends SqlMapClientDaoSupport implements IDao<Concept> 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Concept> getAll() throws DataAccessException {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Gets all concept");
+		}
 		return this.getSqlMapClientTemplate().queryForList(DBRequestsConstants.GET_ALL_CONCEPT);
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int add(Concept concept) throws DataAccessException {
-		return (Integer) this.getSqlMapClientTemplate().insert(DBRequestsConstants.ADD_CONCEPT, concept.getId());
+		if (logger.isDebugEnabled()) {
+			logger.debug("Add concept");
+		}
+		return (Integer) this.getSqlMapClientTemplate().insert(DBRequestsConstants.ADD_CONCEPT, concept.getConcept_id());
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
-	public int save(Concept concept) throws DataAccessException {		
-		return (Integer) this.getSqlMapClientTemplate().update(DBRequestsConstants.SAVE_CONCEPT, concept.getId());
+	public int save(Concept concept) throws DataAccessException {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Save concept");
+		}
+		return (Integer) this.getSqlMapClientTemplate().update(DBRequestsConstants.SAVE_CONCEPT, concept.getConcept_id());
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int delete(Concept concept) throws DataAccessException {
-		return  this.getSqlMapClientTemplate().delete(DBRequestsConstants.DELETE_CONCEPT, concept.getId());
+		if (logger.isDebugEnabled()) {
+			logger.debug("Delete concept");
+		}
+		return  this.getSqlMapClientTemplate().delete(DBRequestsConstants.DELETE_CONCEPT, concept.getConcept_id());
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Concept check(String name) throws DataAccessException {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Check concept");
+		}
 		return (Concept) this.getSqlMapClientTemplate().queryForObject(DBRequestsConstants.CHECK_CONCEPT, name);
 	}
 
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Concept getByName(String name) throws DataAccessException {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Gets concept by name");
+		}
 		return (Concept) this.getSqlMapClientTemplate().queryForObject(DBRequestsConstants.GET_CONCEPT_BY_NAME, name);
 	}
 	
