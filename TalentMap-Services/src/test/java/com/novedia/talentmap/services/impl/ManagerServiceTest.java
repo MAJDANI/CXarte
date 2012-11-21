0e170421ec8922a916f0e5a114b55a6742f236dc
Vanessa Guillemain <v.guillemain@novediagroup.com>
2012-11-21 15:57:33 +0000

    [VGU][TMP 2.0] Classes de Test pour ProfileService et ManagerService

---
+++ TalentMap-Services/src/test/java/com/novedia/talentmap/services/impl/ManagerServiceTest.java
@@ -0,0 +1,110 @@
+/**
+ * 
+ */
+package com.novedia.talentmap.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataAccessResourceFailureException;
+
+import com.novedia.talentmap.model.entity.Collaborator;
+import com.novedia.talentmap.store.IDao;
+import com.novedia.talentmap.store.impl.ManagerDao;
+
+/**
+ * @author v.guillemain
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ManagerServiceTest {
+
+	private ManagerService service;
+
+	@Mock
+	private ManagerDao managerDaoMock;
+	
+	@Mock
+	private  IDao<Collaborator> collaboratorDaoMock;
+	
+	private final String DATA_ACCESS_ERROR_MESSAGE = "Data Access Failure";
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		service = new ManagerService();
+		service.setManagerDao(managerDaoMock);
+		service.setCollaboratorDao(collaboratorDaoMock);
+	}
+
+	@Test
+	public void getAllCollaboratorsByManagerIdReturnsAListOfCollaborators() {
+		//Given
+		Integer managerId = 1;
+		Collaborator collaborator = new Collaborator();
+		collaborator.setManager_id(managerId);
+		
+		List<Collaborator> listCollaboratorRequested = new ArrayList<Collaborator>();
+		listCollaboratorRequested.add(collaborator);
+		
+		//When
+		Mockito.when(managerDaoMock.getAllCollaboratorsByManagerId(managerId)).thenReturn(listCollaboratorRequested);
+		List<Collaborator> listCollaboratorActual = service.getAllCollaboratorsByManagerId(managerId);
+		
+		//Then
+		Mockito.verify(managerDaoMock, Mockito.times(1)).getAllCollaboratorsByManagerId(managerId);
+		Assert.assertNotNull(listCollaboratorActual);
+		Assert.assertSame(listCollaboratorRequested, listCollaboratorActual);
+		
+	}
+	
+	@Test (expected=DataAccessException.class)
+	public void getAllCollaboratorsByManagerIdThrowsDataAccessException() throws DataAccessException {
+		//Given
+		Integer managerId = 1;
+		
+		//When
+		Mockito.when(managerDaoMock.getAllCollaboratorsByManagerId(managerId)).thenThrow(new DataAccessResourceFailureException(DATA_ACCESS_ERROR_MESSAGE));
+		service.getAllCollaboratorsByManagerId(managerId);
+		
+	}
+	
+	 @Test
+	public void getCollaboratorReturnsACollaborator() {
+		 //Given
+		 Integer collaboratorId = 1;
+		 Collaborator collaboratorExpected = new Collaborator();
+		 collaboratorExpected.setId(collaboratorId);
+		 
+		 //When
+		 Mockito.when(collaboratorDaoMock.get(collaboratorId)).thenReturn(collaboratorExpected);
+		 Collaborator collaboratorActual = service.getCollaborator(collaboratorId);
+		
+		 //Test
+		 Mockito.verify(collaboratorDaoMock, Mockito.times(1)).get(collaboratorId);
+		 Assert.assertNotNull(collaboratorActual);
+		 Assert.assertSame(collaboratorExpected, collaboratorActual);
+		 
+	}
+	 
+	 @Test(expected=DataAccessException.class)
+	public void getCollaboratorThrowsDataAccessException () throws DataAccessException {
+		//Given
+		 Integer collaboratorId = 1;
+
+		 //When
+		 Mockito.when(collaboratorDaoMock.get(collaboratorId)).thenThrow(new DataAccessResourceFailureException(DATA_ACCESS_ERROR_MESSAGE));
+		 service.getCollaborator(collaboratorId);
+		 
+	} 
+}
---
+++ TalentMap-Services/src/test/java/com/novedia/talentmap/services/impl/ProfileServiceTest.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package com.novedia.talentmap.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataAccessResourceFailureException;
+
+import com.novedia.talentmap.model.entity.Profile;
+import com.novedia.talentmap.store.IDao;
+
+/**
+ * @author v.guillemain
+ *
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ProfileServiceTest {
+
+	private ProfileService service;
+	
+	@Mock
+	private IDao<Profile> profileDaoMock;
+
+	private final String DATA_ACCESS_ERROR_MESSAGE = "Data Access Failure";
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		service = new ProfileService();
+		service.setProfileDao(profileDaoMock);
+	}
+	
+
+	@Test
+	public void getAllProfileReturnsAListOfProfiles() {
+		//Given
+		Profile profileExpected = new Profile();
+		List<Profile> listProfileExpected = new ArrayList<Profile>();
+		listProfileExpected.add(profileExpected);
+		
+		//When
+		Mockito.when(profileDaoMock.getAll()).thenReturn(listProfileExpected);
+		List<Profile> listProfileActual = service.getAllProfile();
+		
+		//Then
+		Mockito.verify(profileDaoMock, Mockito.times(1)).getAll();
+		Assert.assertNotNull(listProfileActual);
+		Assert.assertSame(listProfileExpected, listProfileActual);
+		
+	}
+
+	@Test(expected=DataAccessException.class)
+	public void getAllProfileThrowsDataAccessException() throws DataAccessException {
+		//When
+		Mockito.when(profileDaoMock.getAll()).thenThrow(new DataAccessResourceFailureException(DATA_ACCESS_ERROR_MESSAGE));
+		service.getAllProfile();
+		
+	}
+	
+	@Test
+	public void getProfileReturnsAProfile() {
+		//Given
+		Integer profileId = 1;
+		Profile profileExpected = new Profile();
+		profileExpected.setId(profileId);
+		
+		//When
+		Mockito.when(profileDaoMock.get(profileId)).thenReturn(profileExpected);
+		Profile profileActual = service.getProfile(profileId);
+		
+		//Then
+		Mockito.verify(profileDaoMock, Mockito.times(1)).get(profileId);
+		Assert.assertNotNull(profileActual);
+		Assert.assertSame(profileExpected, profileActual);
+		
+	}
+	
+	@Test(expected=DataAccessException.class)
+	public void getProfileThrowsDataAccessException() throws DataAccessException{
+		//Given
+		Integer profileId = 1;
+		
+		//When
+		Mockito.when(profileDaoMock.get(profileId)).thenThrow(new DataAccessResourceFailureException(DATA_ACCESS_ERROR_MESSAGE));
+		service.getProfile(profileId);
+		
+	}
+}
