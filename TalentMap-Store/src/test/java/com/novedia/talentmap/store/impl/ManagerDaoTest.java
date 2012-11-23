7b958a9a4921a2099b29a97534156b1de881c9e4
Elise Moumbe <e.moumbe@novediagroup.com>
2012-11-23 17:51:06 +0000

    EMO: Fichier de test

---
+++ TalentMap-Store/src/test/java/com/novedia/talentmap/store/impl/ManagerDaoTest.java
@@ -0,0 +1,40 @@
+package com.novedia.talentmap.store.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.unitils.UnitilsJUnit4;
+import org.unitils.dbunit.annotation.DataSet;
+import org.unitils.dbunit.datasetloadstrategy.impl.CleanInsertLoadStrategy;
+import org.unitils.reflectionassert.ReflectionAssert;
+import org.unitils.spring.annotation.SpringApplicationContext;
+import org.unitils.spring.annotation.SpringBeanByName;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.novedia.talentmap.model.entity.Manager;
+
+/**
+ * Test ManagerDao
+ * @author moumbe
+ *
+ */
+@DataSet (loadStrategy = CleanInsertLoadStrategy.class)
+@SpringApplicationContext("test-store-spring-context.xml")
+public class ManagerDaoTest extends UnitilsJUnit4 {
+
+	@SpringBeanByName
+	private SqlMapClient sqlMapClient;
+	
+	@SpringBeanByName
+	private ManagerDao managerDao;
+	
+	@Before
+	public void setUp() {
+		managerDao.setSqlMapClient(sqlMapClient);
+	}
+	
+	@Test
+	public void testGet () {
+		Manager manager = managerDao.get(1);
+		ReflectionAssert.assertPropertyReflectionEquals("id", 1, manager);
+	}
+}
