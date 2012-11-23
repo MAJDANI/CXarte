8f18e5ef87f99a8f175e8a93316b8f3dd4940a5a
Elise Moumbe <e.moumbe@novediagroup.com>
2012-11-23 17:55:04 +0000

    EMO: bean

---
+++ TalentMap-Model/src/main/java/com/novedia/talentmap/model/entity/ObjectiveTask.java
@@ -0,0 +1,16 @@
+package com.novedia.talentmap.model.entity;
+
+import java.io.Serializable;
+
+/**
+ * Contient les données de la table de relation entre les Objectifs et les
+ * différentes tâches à executer
+ * @author moumbe
+ *
+ */
+public class ObjectiveTask implements Serializable {
+
+	/**UID*/
+	private static final long serialVersionUID = 1L;
+
+}
