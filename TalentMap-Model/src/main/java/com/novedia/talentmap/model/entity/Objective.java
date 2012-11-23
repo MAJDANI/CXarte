829569e6e1afee1c64e9faa00f5e460e09704ff4
Elise Moumbe <e.moumbe@novediagroup.com>
2012-11-23 17:54:42 +0000

    EMO: bean

---
+++ TalentMap-Model/src/main/java/com/novedia/talentmap/model/entity/Objective.java
@@ -0,0 +1,15 @@
+package com.novedia.talentmap.model.entity;
+
+import java.io.Serializable;
+
+/**
+ * The {@link Objective} holds data from table Objective
+ * @author moumbe
+ *
+ */
+public class Objective implements Serializable {
+
+	/**UID*/
+	private static final long serialVersionUID = 1L;
+
+}
