19ca97f4259a7b6baacf1a6aea88911fa41e9643
Elise Moumbe <e.moumbe@novediagroup.com>
2012-11-23 18:02:44 +0000

    EMO: tests

--- TalentMap-Store/src/test/java/com/novedia/talentmap/store/impl/ToolDaoTest.java
+++ TalentMap-Store/src/test/java/com/novedia/talentmap/store/impl/ToolDaoTest.java
@@ -1,40 +1,40 @@
-//package com.novedia.talentmap.store.impl;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.unitils.UnitilsJUnit4;
-//import org.unitils.dbunit.annotation.DataSet;
-//import org.unitils.dbunit.datasetloadstrategy.impl.CleanInsertLoadStrategy;
-//import org.unitils.reflectionassert.ReflectionAssert;
-//import org.unitils.spring.annotation.SpringApplicationContext;
-//import org.unitils.spring.annotation.SpringBean;
-//
-//import com.ibatis.sqlmap.client.SqlMapClient;
-//import com.novedia.talentmap.model.entity.Tool;
-//
-///**
-// * Test ToolDao
-// * @author moumbe
-// *
-// */
-//@DataSet (loadStrategy = CleanInsertLoadStrategy.class)
-//@SpringApplicationContext("test-store-spring-context.xml")
-//public class ToolDaoTest extends UnitilsJUnit4 {
-//
-//	@SpringBean("sqlMapClient")
-//	private SqlMapClient sqlMapClient;
-//	
-//	@SpringBean("toolDao")
-//	private ToolDao toolDao;
-//	
-//	@Before
-//	public void setUp() {
-//		toolDao.setSqlMapClient(sqlMapClient);
-//	}
-//	
-//	@Test
-//	public void testGet() {
-//		Tool tool = toolDao.get(1);
-//		ReflectionAssert.assertPropertiesNotNull("object is not null", tool);
-//	}
-//}
+package com.novedia.talentmap.store.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.unitils.UnitilsJUnit4;
+import org.unitils.dbunit.annotation.DataSet;
+import org.unitils.dbunit.datasetloadstrategy.impl.CleanInsertLoadStrategy;
+import org.unitils.reflectionassert.ReflectionAssert;
+import org.unitils.spring.annotation.SpringApplicationContext;
+import org.unitils.spring.annotation.SpringBean;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.novedia.talentmap.model.entity.Tool;
+
+/**
+ * Test ToolDao
+ * @author moumbe
+ *
+ */
+@DataSet (loadStrategy = CleanInsertLoadStrategy.class)
+@SpringApplicationContext("test-store-spring-context.xml")
+public class ToolDaoTest extends UnitilsJUnit4 {
+
+	@SpringBean("sqlMapClient")
+	private SqlMapClient sqlMapClient;
+	
+	@SpringBean("toolDao")
+	private ToolDao toolDao;
+	
+	@Before
+	public void setUp() {
+		toolDao.setSqlMapClient(sqlMapClient);
+	}
+	
+	@Test
+	public void testGet() {
+		Tool tool = toolDao.get(1);
+		ReflectionAssert.assertPropertyLenientEquals("id", 1, tool);
+	}
+}
