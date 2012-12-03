e0bb148f15064d037f3e70b5dbe2c616d21296ba
Elise Moumbe <e.moumbe@novediagroup.com>
2012-12-03 11:40:34 +0000

    EMO: j'ai rajouté la requete get qui retourne un skillcollab par son id

--- TalentMap-Store/src/main/java/com/novedia/talentmap/store/IVSkillCollabDao.java
+++ TalentMap-Store/src/main/java/com/novedia/talentmap/store/IVSkillCollabDao.java
@@ -5,18 +5,29 @@ import java.util.List;
 import org.springframework.dao.DataAccessException;
 
 import com.novedia.talentmap.model.entity.VSkillCollab;
+
 /**
- * @author v.dibi
+ * The interface {@link IVSkillCollabDao}
+ * @author moumbe
  *
  */
 public interface IVSkillCollabDao {
-
-/**
- * Get all Collaborators' Skill by ID.
- * @class IVSkillCollabDao.java
- * @param collabId a id
- * @return a list VSkillCollab
- */
-List<VSkillCollab> getAllSkillCollab(int collabId);
-
+	
+	/**
+	 * Get Skill by Id
+	 * @param id
+	 * @return
+	 * @throws DataAccessException
+	 */
+	public VSkillCollab get(int id) throws DataAccessException;
+	
+	/**
+	 * Get all Collaborators' Skill by ID 
+	 * @class IVSkillCollabDao.java
+	 * @param collab_id
+	 * @return
+	 * @throws Exception
+	 */
+	List<VSkillCollab> getAllSkillCollab(int collab_id) throws DataAccessException;
+	
 }
