b05607979481635438f9f6094be7ddd09c1bdb5c
Elise Moumbe <e.moumbe@novediagroup.com>
2012-12-17 15:04:45 +0000

    EMO: Refactoring du nom de la classe + suppression des méthodes non utilisées.

---
+++ TalentMap-Services/src/main/java/com/novedia/talentmap/services/impl/AuthenticationService.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.novedia.talentmap.services.impl;
+
+import com.novedia.talentmap.model.entity.Authentication;
+import com.novedia.talentmap.model.entity.CredentialToken;
+import com.novedia.talentmap.services.IAuthenticationService;
+import com.novedia.talentmap.store.IDao;
+import com.novedia.talentmap.store.impl.AuthenticationDao;
+
+/**
+ * This {@link AuthenticationService} authenticates user
+ * @author v.dibi
+ *
+ */
+public class AuthenticationService implements IAuthenticationService {
+
+	/**
+	 * The authentification DAO.
+	 */
+	private IDao<Authentication> authenticationDao;
+
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.novedia.talentmap.services.IAuthentificationService#checkAuthentification(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public Authentication checkUser(CredentialToken token) {
+		AuthenticationDao authentificationDao = (AuthenticationDao) this.authenticationDao;
+		return authentificationDao.check(token);
+	}
+
+
+	/**
+	 * @param authenticationDao the authenticationDao to set
+	 */
+	public void setAuthenticationDao(IDao<Authentication> authenticationDao) {
+		this.authenticationDao = authenticationDao;
+	}
+
+
+}
