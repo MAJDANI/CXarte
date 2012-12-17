f6ddef677e2896a0a873acafd86826d01f9def78
Jean-Max Marie-Sainte <j.marie-sainte@novediagroup.com>
2012-12-17 17:17:44 +0000

    [JMS][2.0]: Java Doc Tmp-Services.

--- TalentMap-Services/src/main/java/com/novedia/talentmap/services/impl/ProfileService.java
+++ TalentMap-Services/src/main/java/com/novedia/talentmap/services/impl/ProfileService.java
@@ -10,52 +10,62 @@ import com.novedia.talentmap.store.IDao;
 
 /**
  * Profile Services.
+ * 
  * @author j.collet
  */
 public class ProfileService implements IProfileService {
-/**
- * profile DAO.
- */
-private IDao<Profile> profileDao;
-/**
- *  get profile DAO.
- * @return
- */
-public IDao<Profile> getProfileDao() {
-return profileDao;
-}
-/**
- *  This method allowd to make the spring injection.
- * @param profileDao
- */
-public void setProfileDao(IDao<Profile> profileDao) {
-this.profileDao = profileDao;
-}
-/**
- * @return a list of profile.
- * @throws DataAccessException
- */
-@Override
-public List<Profile> getAllProfile() throws DataAccessException {
-return profileDao.getAll();
-}
+	/**
+	 * profile DAO.
+	 */
+	private IDao<Profile> profileDao;
 
-/**
- * @return a profile by id.
- * @throws DataAccessException
- */
-@Override
-public Profile getProfile(Integer id) throws DataAccessException {	
-return profileDao.get(id);
-}
+	/**
+	 * get profile DAO.
+	 * 
+	 * @return
+	 */
+	public IDao<Profile> getProfileDao() {
+		return profileDao;
+	}
 
-/**
- * @return type.
- * @throws DataAccessException
- */
-@Override
-public Profile getProfile(String type) throws DataAccessException {
-// TODO Auto-generated method stub
-return null;
-}
+	/**
+	 * This method allowd to make the spring injection.
+	 * 
+	 * @param profileDao
+	 */
+	public void setProfileDao(IDao<Profile> profileDao) {
+		this.profileDao = profileDao;
+	}
+
+	/**
+	 * @return a list of profile.
+	 * @throws DataAccessException
+	 */
+	@Override
+	public List<Profile> getAllProfile() throws DataAccessException {
+		return profileDao.getAll();
+	}
+
+	/**
+	 * Get the profile 
+	 * 
+	 * @return a profile.
+	 * @throws DataAccessException
+	 */
+	@Override
+	public Profile getProfile(Integer id) throws DataAccessException {
+		return profileDao.get(id);
+	}
+
+	/**
+	 * Get the profile by type
+	 * 
+	 * @return a profile.
+	 * @throws DataAccessException
+	 */
+	@Override
+	public Profile getProfile(String type) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
