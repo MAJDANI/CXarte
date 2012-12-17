2fb825761ef866d8a54d2746b24df3239af77f9c
Elise Moumbe <e.moumbe@novediagroup.com>
2012-12-17 15:37:44 +0000

    EMO: Classe qui définis les constantes des labels des écrans.

---
+++ TalentMap-Web/src/main/java/com/novedia/talentmap/web/util/LabelConstants.java
@@ -0,0 +1,25 @@
+package com.novedia.talentmap.web.util;
+
+/**
+ * The constants for labels
+ * @author e.moumbe
+ *
+ */
+public class LabelConstants {
+
+	/**
+	 * Constant for login
+	 */
+	public static final String USER_LOGIN = "login";
+	
+	/**
+	 * Constant for password
+	 */
+	public static final String USER_PASSWORD = "password";
+	
+	/**
+	 * Constant label for button
+	 */
+	public static final String LOGIN_BUTTON = "Log In";
+	
+}
