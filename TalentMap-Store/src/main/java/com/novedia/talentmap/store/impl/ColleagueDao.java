c78e856950fd80c7a604e8f10b1f194d164de6e0
Jean-Max Marie-Sainte <j.marie-sainte@novediagroup.com>
2012-12-17 17:23:28 +0000

    [JMS][2.0]: Java Doc Tmp-Store - Refactoring ColleagueDao remplace CollaboratorDao.

---
+++ TalentMap-Store/src/main/java/com/novedia/talentmap/store/impl/ColleagueDao.java
@@ -0,0 +1,74 @@
+package com.novedia.talentmap.store.impl;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.orm.ibatis.support.SqlMapClientDaoSupport;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.novedia.talentmap.model.entity.Colleague;
+import com.novedia.talentmap.store.IDao;
+import com.novedia.talentmap.store.utils.DBRequestsConstants;
+
+/**
+ * The Colleague DAO.
+ * 
+ * @author j.collet
+ */
+public class ColleagueDao extends SqlMapClientDaoSupport implements IDao<Colleague>{
+	
+	/**
+	 * The logger
+	 */
+	private static Log logger = LogFactory.getLog(ColleagueDao.class);
+	
+	/**
+	 * Class builder based on sqlMapClient
+	 * @param sqlMapClient
+	 */
+	public ColleagueDao(final SqlMapClient sqlMapClient){
+		setSqlMapClient(sqlMapClient);
+	}
+	
+	@Override
+	public Colleague get(Integer id) throws DataAccessException {
+		return (Colleague) this.getSqlMapClientTemplate().queryForObject(DBRequestsConstants.GET_COLLEAGUE, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Colleague> getAll() throws DataAccessException {
+		return this.getSqlMapClientTemplate().queryForList(DBRequestsConstants.GET_ALL_COLLEAGUES);
+	}
+
+	@Override
+	public int save(Colleague colleague) throws DataAccessException {
+		return this.getSqlMapClientTemplate().update(DBRequestsConstants.SAVE_COLLEAGUE, colleague);
+	}
+
+	@Override
+	public int add(Colleague colleague) throws DataAccessException {
+		if (logger.isDebugEnabled()) {
+			logger.debug("Add colleague");
+		}
+		return (Integer) this.getSqlMapClientTemplate().insert(DBRequestsConstants.ADD_COLLEAGUE, colleague);
+	}
+
+	@Override
+	public int delete(Colleague collaborator) throws DataAccessException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Colleague getByName(String name) throws DataAccessException {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public Colleague check(String name) throws DataAccessException {
+		throw new UnsupportedOperationException();
+	}
+
+}
