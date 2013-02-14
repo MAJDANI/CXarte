15d73723aa00d467a930a3c20536fbcd45bc0909
Jean-Max Marie-Sainte <j.marie-sainte@novediagroup.com>
2013-02-14 15:23:37 +0000

    [JMS][2.0]: Enrichit une classe BusinessEngineer avec de nouveaux attributs.

--- TalentMap-Model/src/main/java/com/novedia/talentmap/model/entity/BusinessEngineer.java
+++ TalentMap-Model/src/main/java/com/novedia/talentmap/model/entity/BusinessEngineer.java
@@ -2,14 +2,11 @@ package com.novedia.talentmap.model.entity;
 
 import java.io.Serializable;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-
-import com.novedia.talentmap.model.entity.Colleague.Builder;
-
 /**
  * This entity represents a Business Engineer.
  * 
  * @author v.guillemain
+ * @author j.marie-sainte
  */
 public class BusinessEngineer implements Serializable {
 
@@ -21,20 +18,127 @@ public class BusinessEngineer implements Serializable {
 	/**
 	 * Entity identifier
 	 */
-	protected Integer id;
+	private Integer id;
+	
+	/**
+	 * Business Engineer first name
+	 */
+	private String firstName;
 	
 	/**
 	 * Business Engineer last name
 	 */
-	protected String name;
+	private String lastName;
 	
 	/**
-	 * Build an immutable category entity. the builder inner class for this
-	 * entity
+	 * Business Unit
+	 */
+	private String businessUnit;
+	
+	/**
+	 * Default constructor
 	 */
 	public BusinessEngineer() {
+	
+	}
+	
+	/**
+	 * Gets the Business Engineer's Id
+	 * @return the id of the Business Engineer
+	 */
+	public Integer getId() {
+		return id;
 	}
 
+	/**
+	 * Sets the Business Engineer's Id
+	 * @param id : the Business Engineer's Id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * Gets the Business Engineer's name
+	 * @return the name of the Business Engineer
+	 */
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	/**
+	 * Sets the Business Engineer's firs name
+	 * 
+	 * @param firstName the Business Engineer's first name to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	/**
+	 * Get the business engineer's last name
+	 * 
+	 * @return lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/**
+	 * Set the last name
+	 * 
+	 * @param lastName the business engineer's last name
+	 */
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * Get the business unit
+	 * 
+	 * @return businessUnit
+	 */
+
+	public String getBusinessUnit() {
+		return businessUnit;
+	}
+	
+	/**
+	 * Set the business unit
+	 * 
+	 * @param businessUnit the business unit
+	 */
+
+	public void setBusinessUnit(String businessUnit) {
+		this.businessUnit = businessUnit;
+	}
+
+	/**
+	 * {@inheritDoc} 
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null || !(obj instanceof BusinessEngineer)){
+			return false;
+		}
+		else {
+			BusinessEngineer b = (BusinessEngineer) obj;
+			return (this.id.equals(b.getId()));
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		StringBuilder strBld = new StringBuilder();
+		strBld.append("[id=").append(getId()).append(", ");
+		strBld.append("name=").append(getFirstName()).append("] ");
+		return strBld.toString();
+	}
+	
 	/**
 	 * Build an immutable Business Engineer entity.
 	 * 
@@ -43,7 +147,9 @@ public class BusinessEngineer implements Serializable {
 	 */
 	public BusinessEngineer(final Builder builder) {
 		this.id = builder.id;
-		this.name = builder.name;
+		this.firstName = builder.firstName;
+		this.lastName = builder.lastName;
+		this.businessUnit = builder.firstName;
 	}
 
 	
@@ -73,9 +179,19 @@ public class BusinessEngineer implements Serializable {
 		private Integer id;
 
 		/**
+		 * Business Engineer first name
+		 */
+		private String firstName;
+		
+		/**
 		 * Business Engineer last name
 		 */
-		protected String name;
+		private String lastName;
+		
+		/**
+		 * The activity market of business engineer
+		 */
+		private String businessUnit;
 
 		/**
 		 * Set id
@@ -89,16 +205,38 @@ public class BusinessEngineer implements Serializable {
 		}
 
 		/**
-		 * Set name
+		 * Set firstName
 		 * 
 		 * @param name the Business Engineer's name
 		 * @return the builder
 		 */
-		public Builder name(final String name) {
-			this.name = name;
+		public Builder firstName(final String name) {
+			this.firstName = name;
 			return this;
 		}
-
+		
+		/**
+		 * Set lastName
+		 * 
+		 * @param name the Business Engineer's name
+		 * @return the builder
+		 */
+		public Builder lastName(final String lastName) {
+			this.lastName = lastName;
+			return this;
+		}
+		
+		/**
+		 * Set lastName
+		 * 
+		 * @param name the Business Engineer's name
+		 * @return the builder
+		 */
+		public Builder businessUnit(final String businessUnit) {
+			this.businessUnit = businessUnit;
+			return this;
+		}
+		
 		/**
 		 * Build an immutable instance of BusinessEngineer.
 		 * @return BusinessEngineer
@@ -115,65 +253,5 @@ public class BusinessEngineer implements Serializable {
 			return new Builder();
 		}
 	}
-	
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		StringBuilder strBld = new StringBuilder();
-		strBld.append("[id=").append(getId()).append(", ");
-		strBld.append("name=").append(getName()).append("] ");
-		return strBld.toString();
-	}
-
-	/**
-	 * Gets the Business Engineer's Id
-	 * @return the id of the Business Engineer
-	 */
-	public Integer getId() {
-		return id;
-	}
-
-	/**
-	 * Sets the Business Engineer's Id
-	 * @param id : the Business Engineer's Id to set
-	 */
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	/**
-	 * Gets the Business Engineer's name
-	 * @return the name of the Business Engineer
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * Sets the Business Engineer's name
-	 * @param name : the Business Engineer's name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if(obj == null || !(obj instanceof BusinessEngineer)){
-			return false;
-		}else {
-			BusinessEngineer b = (BusinessEngineer) obj;
-			return (this.id.equals(b.getId()) && this.name.equals(b.getName()));
-		}
-	}
-
 
 }
