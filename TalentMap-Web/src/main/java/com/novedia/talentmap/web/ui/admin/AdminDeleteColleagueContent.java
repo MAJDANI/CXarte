6def55b9f8c506356155a86d512f8920fd772a4d
Boris Tiomo <b.tiomofofou@novediagroup.com>
2013-03-20 17:39:15 +0000

    [TBO][2.1] Administration : suppression des collègues; rendre l'affichage identique celle de la recheche des collaborateurs

--- TalentMap-Web/src/main/java/com/novedia/talentmap/web/ui/admin/AdminDeleteColleagueContent.java
+++ TalentMap-Web/src/main/java/com/novedia/talentmap/web/ui/admin/AdminDeleteColleagueContent.java
@@ -1,23 +1,20 @@
 package com.novedia.talentmap.web.ui.admin;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import com.jensjansson.pagedtable.PagedTable;
-import com.jensjansson.pagedtable.PagedTableContainer;
 import com.novedia.talentmap.model.entity.Colleague;
 import com.novedia.talentmap.services.IAdminService;
 import com.novedia.talentmap.services.IColleagueService;
 import com.novedia.talentmap.web.util.CUtils;
-import com.vaadin.data.Container.Filter;
-import com.vaadin.data.Container.Filterable;
-import com.vaadin.data.Item;
+import com.novedia.talentmap.web.util.TalentMapCSS;
 import com.vaadin.data.Property;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
-import com.vaadin.data.util.IndexedContainer;
-import com.vaadin.data.util.filter.UnsupportedFilterException;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.event.FieldEvents.TextChangeEvent;
 import com.vaadin.event.FieldEvents.TextChangeListener;
 import com.vaadin.ui.AbstractSelect.MultiSelectMode;
@@ -26,11 +23,12 @@ import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
-public class AdminDeleteColleagueContent extends VerticalLayout implements ClickListener ,ValueChangeListener{
+public class AdminDeleteColleagueContent extends VerticalLayout implements ClickListener ,ValueChangeListener, TextChangeListener{
 
 	/**
 	 * 
@@ -44,21 +42,16 @@ public class AdminDeleteColleagueContent extends VerticalLayout implements Click
 	
 	private IColleagueService colleagueService;
 	
-	private static final String[] colleagueProperties = new String[]{"ID","ManagerID","ProfileID","First Name",
-		"Last Name","Email","Phone"};
-	
-	private static final String[] VisibleColumn = {colleagueProperties[3],colleagueProperties[4],colleagueProperties[5],
-		colleagueProperties[6]};
+	private static final String[] VisibleColumn = {"First Name","Last Name","Email","Phone"};
 	
 	private static final String LABEL_BUTTON_DELETE = "Delete";
 	private static final String LABEL_BUTTON_CANCEL = "Cancel";
 	private static final String LABEL_BUTON_YES = "Yes";
 	private static final String LABEL_BUTON_NO = "No";
 	private static final String CONFIRM_DELETE = "Are you sure you want to delete these colleagues ?";
+	private static final String NO_RESULT_TEXT = "No colleague found";
 	
-	private static final int PAGE_SIZE = 10;
-	
-	private IndexedContainer colleagueContainer ;
+	private static final int PAGE_SIZE = 5;
 	
 	/**
 	 * vaadin componnents
@@ -68,8 +61,12 @@ public class AdminDeleteColleagueContent extends VerticalLayout implements Click
 	private Button cancel ;
 	private PagedTable colleagueList ;
 	private HorizontalLayout containerSearch;
+	private Panel searchPanel;
+	private Panel resultPanel;
 	private TextField searchField ;
 	private Window popup;
+	
+	private BeanItemContainer<Colleague> colleagueContainer;
 	
 	/**
 	 * Default constructor
@@ -78,70 +75,143 @@ public class AdminDeleteColleagueContent extends VerticalLayout implements Click
 		super();
 	}
 	
-	/**
-	 * 
-	 * @param colleagueService
-	 * @param adminService
-	 */
-//	public AdminDeleteColleagueContent(IColleagueService colleagueService,IAdminService adminService){
-//		
-//		this.adminService = adminService;
-//		this.colleagueService = colleagueService;
-//		//mainBuild();
-//	}
-	
-	
 	
 	/**
-	 * 
+	 * Build the view that allow to delete a colleague
 	 */
 	public AdminDeleteColleagueContent mainBuild(){
 		removeAllComponents();
 		setMargin(true);
 		setSpacing(true);
-		builColleagueContainer();
-		initColleagueList();
-		intSearch();
-		addComponent(containerSearch);
-		addComponent(colleagueList);
-		if(colleagueList.size() > 10){
-			HorizontalLayout controls = colleagueList.createControls();
-			addComponent(controls);
-			colleagueList.setPageLength(PAGE_SIZE);
-		}
-		
-		buildButton();
-		addComponent(containerButon);
+		intSearchPannel();
+		initResultPanel();
+		addComponent(searchPanel);
+		resultPanel.setVisible(false);
+		addComponent(resultPanel);
 		return this;
 	}
 	
 	
-	
 	/**
-	 * 
+	 * init search pannel
 	 */
-	public void intSearch(){
-		
-		searchField = new TextField();
-		searchField.addListener(new TextChangeListener() {
-             public void textChange(final TextChangeEvent event) {
-            	 colleagueContainer.removeAllContainerFilters();
-            	 colleagueContainer.addContainerFilter(new ColleagueFilter(event.getText()));
-            	 
-             }
-     });
-		
+	public void intSearchPannel(){
+		searchPanel = new Panel();
+		searchField = new TextField("Colleague Name :");
+		searchField.addListener((TextChangeListener) this);
 		containerSearch = new HorizontalLayout();
 		containerSearch.setSpacing(true);
 		containerSearch.addComponent(searchField);
+		searchPanel.addComponent(containerSearch); 
 		
+	}
+	
+	/**
+	 * init result pannel
+	 */
+	private void initResultPanel(){
+		resultPanel = new Panel();
+		colleagueContainer = new BeanItemContainer<Colleague>(Colleague.class);
+		refreshColleagueTable();
+		buildContainerButton();
+	}
+	
+	
+	
+	@Override
+	public void textChange(final TextChangeEvent event) {
+		String value = event.getText();
+		value = value.trim();
+		if(value.length() != 0){
+			buildResultPanel(value);
+		}
+		
+	}
+	
+	/**
+	 * Build the reult pannel 
+	 * @param value
+	 */
+	private void buildResultPanel(String value){
+		fillColleagueContainer(value);
+		fillColleagueList();
+		refreshResultPanel();
+	}
+	
+	/**
+	 * Fill the container of table  
+	 * @param name
+	 */
+	private void fillColleagueContainer(String name){
+		List<Colleague> colleagueResult = colleagueService.getAllColleaguesByLastName(name);
+		colleagueContainer.removeAllItems();
+		if(colleagueResult != null && !colleagueResult.isEmpty()){
+			for (Colleague colleague : colleagueResult) {
+				colleagueContainer.addBean(colleague);
+			}
+		}
+		
+	}
+	
+	/**
+	 * Refresh table's colleague
+	 */
+	private void refreshColleagueTable(){
+		colleagueList = null;
+		colleagueList = new PagedTable();
+		for (String property : VisibleColumn) {
+			colleagueList.addContainerProperty(property, String.class,"");
+		}
+		colleagueList.setSelectable(true);
+		colleagueList.setImmediate(true);
+		colleagueList.setMultiSelect(true);
+		colleagueList.setMultiSelectMode(MultiSelectMode.SIMPLE);
+		colleagueList.setWidth("100%");
+		colleagueList.setNullSelectionAllowed(true);
+		colleagueList.addListener((Property.ValueChangeListener) this);
+	}
+	
+	/**
+	 * fill the table to display
+	 */
+	private void fillColleagueList(){
+		refreshColleagueTable();
+		Collection<Colleague> colleagueResult = colleagueContainer.getItemIds();
+		for (Colleague colleague : colleagueResult) {
+			colleagueList.addItem(new Object[]{colleague.getFirstName(),colleague.getLastName(),
+					colleague.getEmail(),colleague.getPhone()},colleague);
+		}
+	}
+	
+	/**
+	 * Refresh the view of result pannel
+	 */
+	private void refreshResultPanel(){
+		resultPanel.removeAllComponents();
+		resultPanel.setVisible(true);
+		if(colleagueContainer.size() > 0){
+			Label resultTitle = new Label("Colleague List");
+			resultTitle.addStyleName(TalentMapCSS.H2);
+			resultPanel.addComponent(resultTitle);
+			resultPanel.addComponent(colleagueList);
+			if(colleagueContainer.size() > PAGE_SIZE){
+				resultPanel.addComponent(colleagueList.createControls());
+				colleagueList.setPageLength(PAGE_SIZE);
+			}
+			resultPanel.addComponent(containerButon);
+			
+		} else {
+			Label noResultText = new Label(NO_RESULT_TEXT);
+			noResultText.addStyleName(TalentMapCSS.H2);
+			resultPanel.addComponent(noResultText);
+		}
 	}
 	
 	
 	/**
-	 * 
+	 * Build container of button
 	 */
-	public void buildButton(){
+	private void buildContainerButton(){
 		containerButon = new HorizontalLayout();
 		containerButon.setSpacing(true);
 		delete = new Button(LABEL_BUTTON_DELETE);
@@ -157,47 +227,6 @@ public class AdminDeleteColleagueContent extends VerticalLayout implements Click
 		
 	}
 	
-	
-	/**
-	 * table to display
-	 */
-	public void initColleagueList(){
-		colleagueList = new PagedTable("");
-		colleagueList.setContainerDataSource(colleagueContainer);
-		colleagueList.setVisibleColumns(VisibleColumn);
-		colleagueList.setSelectable(true);
-		colleagueList.setImmediate(true);
-		colleagueList.setMultiSelect(true);
-		colleagueList.setMultiSelectMode(MultiSelectMode.SIMPLE);
-		colleagueList.setWidth("100%");
-		colleagueList.setNullSelectionAllowed(true);
-		colleagueList.addListener((Property.ValueChangeListener) this);
-	}
-	
-	/**
-	 * fill all colleague in the container
-	 */
-	public void builColleagueContainer(){
-		List<Colleague> allColleague = colleagueService.getAllColleagues();
-		if (allColleague != null) {
-			colleagueContainer = new IndexedContainer();
-			for (String property : colleagueProperties) {
-				colleagueContainer.addContainerProperty(property, String.class,"");
-			}
-			
-			for (Colleague colleague : allColleague) {
-				Object id = colleagueContainer.addItem();
-				colleagueContainer.getContainerProperty(id, colleagueProperties[0]).setValue(colleague.getId());
-				colleagueContainer.getContainerProperty(id, colleagueProperties[1]).setValue(colleague.getManagerId());
-				colleagueContainer.getContainerProperty(id, colleagueProperties[2]).setValue(colleague.getProfileId());
-				colleagueContainer.getContainerProperty(id, colleagueProperties[3]).setValue(colleague.getFirstName());
-				colleagueContainer.getContainerProperty(id, colleagueProperties[4]).setValue(colleague.getLastName());
-				colleagueContainer.getContainerProperty(id, colleagueProperties[5]).setValue(colleague.getEmail());
-				colleagueContainer.getContainerProperty(id, colleagueProperties[6]).setValue(colleague.getPhone());
-			}
-		} 		
-		
-	}
 
 	/**
 	 * build pop up confirmation
@@ -218,7 +247,6 @@ public class AdminDeleteColleagueContent extends VerticalLayout implements Click
 		popup.addComponent(new Label(CONFIRM_DELETE));
 		popup.addComponent(confirmationButtonContainer);
 		popup.setModal(true);
-		
 		getWindow().addWindow(popup);
 	}
 	
@@ -236,21 +264,16 @@ public class AdminDeleteColleagueContent extends VerticalLayout implements Click
 			getWindow().removeWindow(popup);
 		} else if (event.getButton().getCaption().equals(LABEL_BUTON_YES)) {  // yes button
 			getWindow().removeWindow(popup);
-			Set<Object> setItemSelected = (Set<Object>) colleagueList.getValue();
+			Set<Colleague> colleagueSelected = (Set<Colleague>) colleagueList.getValue();
 			Map<String, Object> mapNotification = null;
-			for (Object itemId : setItemSelected) {
-				int colleagueId =  Integer.parseInt((String) colleagueList.getItem(itemId).getItemProperty(colleagueProperties[0]).getValue());
-				 mapNotification = adminService.deleteColleague(colleagueId);
-				int typeError =(Integer) mapNotification.get("typeError");
-				if( typeError == 1){
-					colleagueList.removeItem(itemId);
-				}
+			for (Colleague colleague : colleagueSelected) {
+				 mapNotification = adminService.deleteColleague(colleague.getId());
 			}
 			
 			if(mapNotification != null){
 				CUtils.showMessage(mapNotification, getWindow());
 			}
-			mainBuild();
+			buildResultPanel((String) searchField.getValue());
 			
 		}
 		
@@ -259,7 +282,7 @@ public class AdminDeleteColleagueContent extends VerticalLayout implements Click
 
 	@Override
 	public void valueChange(ValueChangeEvent event) {
-		Set<Object> setItemSelected = (Set<Object>) colleagueList.getValue();
+		Set<Colleague> setItemSelected = (Set<Colleague>) colleagueList.getValue();
 		  if (setItemSelected != null && setItemSelected.size() != 0){
 			  delete.setEnabled(true);
 			  cancel.setEnabled(true);
@@ -269,78 +292,37 @@ public class AdminDeleteColleagueContent extends VerticalLayout implements Click
 		  }
 	}
 	
-	
-	
-	
-	 private class ColleagueFilter  implements Filter {
-         private String neededValue;
-
-         public ColleagueFilter(String neededValue) {
-                 this.neededValue = neededValue.toLowerCase();
-                 
-         }
-         
-         @Override
-         public boolean passesFilter(Object itemId, Item item) {
-        	
-                 String haystack = ("" + item.getItemProperty(colleagueProperties[3]).getValue()
-                                 + item.getItemProperty(colleagueProperties[4]).getValue()).toLowerCase();
-                 return haystack.contains(neededValue);
-         }
-
-         public boolean appliesToProperty(Object id) {
-                 return true;
-         }
-
-		
-	 }
-	 
-	 private class ColleagueFilter2 extends PagedTableContainer implements Filterable{
-		 
-
-		public ColleagueFilter2(Indexed container) {
-			super(container);
-			
-		}
-
-		@Override
-		public void addContainerFilter(Filter filter)
-				throws UnsupportedFilterException {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void removeContainerFilter(Filter filter) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void removeAllContainerFilters() {
-			// TODO Auto-generated method stub
-			
-		}
-		 
-	 }
-
+	/**
+	 * Get the adminService
+	 * @return adminService
+	 */
 	public IAdminService getAdminService() {
 		return adminService;
 	}
-
+	
+	/**
+	 * Set the adminService
+	 * @param adminService
+	 */
 	public void setAdminService(IAdminService adminService) {
 		this.adminService = adminService;
 	}
 
+	/**
+	 * Get the colleagueService
+	 * @return colleagueService
+	 */
 	public IColleagueService getColleagueService() {
 		return colleagueService;
 	}
 
+	/**
+	 * Set the colleagueService
+	 * @param colleagueService
+	 */
 	public void setColleagueService(IColleagueService colleagueService) {
 		this.colleagueService = colleagueService;
 	}
-
-	
 	
 	
 }
