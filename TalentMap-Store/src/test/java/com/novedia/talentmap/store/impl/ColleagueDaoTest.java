71522d8138238375b468f83099fb490a434b3456
Johann Maquin <j.maquin@novediagroup.com>
2013-04-03 13:46:25 +0000

    [JMA][TMP 2.1]Test Unitaire ColleagueDaoTest

--- TalentMap-Store/src/test/java/com/novedia/talentmap/store/impl/ColleagueDaoTest.java
+++ TalentMap-Store/src/test/java/com/novedia/talentmap/store/impl/ColleagueDaoTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.dao.DataAccessException;
 import org.unitils.UnitilsJUnit4TestClassRunner;
 import org.unitils.dbunit.annotation.DataSet;
 import org.unitils.reflectionassert.ReflectionAssert;
@@ -20,17 +21,17 @@ import org.unitils.spring.annotation.SpringBeanByName;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.novedia.talentmap.model.entity.BusinessEngineer;
 import com.novedia.talentmap.model.entity.Colleague;
+import com.novedia.talentmap.model.entity.Registration;
 
 /**
- * Test class for Data Access to table COLLEAGUE.
- * {@link ColleagueDao}
+ * Test class for Data Access to table COLLEAGUE. {@link ColleagueDao}
+ * 
  * @author j.marie-sainte
  * 
  */
 @SpringApplicationContext("test-store-spring-context.xml")
 @RunWith(UnitilsJUnit4TestClassRunner.class)
 @DataSet("ColleagueDaoTest.xml")
-@Ignore
 public class ColleagueDaoTest {
 
 	@SpringBeanByName
@@ -44,30 +45,39 @@ public class ColleagueDaoTest {
 		colleagueDao.setSqlMapClient(sqlMapClient);
 	}
 
-	@Test
-	public void testGetByMail() {
-		//Given
-		//When
-		//Then
-	}
-	
-	
+	/**
+	 * Test add colleague from registration
+	 */
 	@Test
 	public void testAddColleagueFromRegistration() {
-		//Given
-		//When
-		//Then
-	}	
-	
-	
-	@Test
-	public void testGetAllConsultantManager() {
-		//Given
-		//When
-		//Then
-	}	
-	
-	
+
+		// Given
+		Registration registration = Registration.Builder.builder().profileId(1)
+				.lastName("colleaguetest").firstName("colleaguetest")
+				.email("t.colleaguetest@novediagroup.com")
+				.employmentDate(new Date()).experience(2).build();
+		Integer expectedResult = 1;
+		// When
+		Integer currentResult = null;
+		if (colleagueDao.addColleagueFromRegistration(registration) != null)
+			currentResult = 1;
+		// Then
+		Assert.assertEquals(expectedResult, currentResult);
+	}
+	/**
+	 * Test add colleague from registration DataAccessException
+	 */
+	@Test(expected = DataAccessException.class)
+	public void testAddColleagueFromRegistrationDataAccessException() {
+
+		Registration registration = Registration.Builder.builder().profileId(1)
+				.lastName("colleaguetest").firstName("colleaguetest")
+				.email("collab1@gmail.com").employmentDate(new Date())
+				.experience(2).build();
+
+		colleagueDao.addColleagueFromRegistration(registration);
+
+	}
 	/**
 	 * Test get colleague by id
 	 */
@@ -75,7 +85,8 @@ public class ColleagueDaoTest {
 	public void testGetById() {
 		Colleague expectedColleague = colleagueDao.get(1);
 		Assert.assertNotNull(expectedColleague);
-		ReflectionAssert.assertPropertyLenientEquals("id", 1, expectedColleague);
+		ReflectionAssert
+				.assertPropertyLenientEquals("id", 1, expectedColleague);
 	}
 
 	/**
@@ -83,22 +94,35 @@ public class ColleagueDaoTest {
 	 */
 	@Test
 	public void testGetAll() {
-		
+
 		// Given
-		BusinessEngineer bizEngineer = BusinessEngineer.builder().id(1).firstName("Julie").lastName("VIGNERON").businessUnit("INDUSTRIE").build();
-		
+		BusinessEngineer bizEngineer = BusinessEngineer.builder().id(1)
+				.firstName("Julie").lastName("VIGNERON")
+				.businessUnit("INDUSTRIE").build();
+
 		// When
 		List<Colleague> listCollabs = colleagueDao.getAll();
-		
+
 		// Then
-		ReflectionAssert.assertPropertyLenientEquals("id", Arrays.asList(1, 2),	listCollabs);
-		ReflectionAssert.assertPropertyLenientEquals("lastName", Arrays.asList("collab1", "collab2"),listCollabs);
-		ReflectionAssert.assertPropertyLenientEquals("firstName", Arrays.asList("collab1", "collab2"),listCollabs);
-		ReflectionAssert.assertPropertyLenientEquals("email", Arrays.asList("collab1@gmail.com", "collab2@yahoo.fr"),listCollabs);
-		ReflectionAssert.assertPropertyLenientEquals("employmentDate", Arrays.asList(new DateTime("2011-12-12").toDate(), new DateTime("2011-12-12").toDate()),listCollabs);
-		ReflectionAssert.assertPropertyLenientEquals("phone", Arrays.asList("6010101010", "6010101010"),listCollabs);
-		ReflectionAssert.assertPropertyLenientEquals("experience", Arrays.asList(5, 5),listCollabs);
-		ReflectionAssert.assertPropertyLenientEquals("businessEngineer", Arrays.asList(bizEngineer.getId(), bizEngineer.getId()),listCollabs);
+		ReflectionAssert.assertPropertyLenientEquals("id", Arrays.asList(1, 2),
+				listCollabs);
+		ReflectionAssert.assertPropertyLenientEquals("lastName",
+				Arrays.asList("collab1", "collab2"), listCollabs);
+		ReflectionAssert.assertPropertyLenientEquals("firstName",
+				Arrays.asList("collab1", "collab2"), listCollabs);
+		ReflectionAssert.assertPropertyLenientEquals("email",
+				Arrays.asList("collab1@gmail.com", "collab2@yahoo.fr"),
+				listCollabs);
+		ReflectionAssert.assertPropertyLenientEquals("employmentDate", Arrays
+				.asList(new DateTime("2011-12-12").toDate(), new DateTime(
+						"2011-12-12").toDate()), listCollabs);
+		ReflectionAssert.assertPropertyLenientEquals("phone",
+				Arrays.asList("6010101010", "6010101010"), listCollabs);
+		ReflectionAssert.assertPropertyLenientEquals("experience",
+				Arrays.asList(5, 5), listCollabs);
+		ReflectionAssert.assertPropertyLenientEquals("businessEngineer",
+				Arrays.asList(bizEngineer.getId(), bizEngineer.getId()),
+				listCollabs);
 	}
 
 	/**
@@ -107,19 +131,17 @@ public class ColleagueDaoTest {
 	@Test
 	@DataSet("ColleagueDaoTest.testSave-result.xml")
 	public void testSave() {
-		BusinessEngineer businessEngineer = BusinessEngineer.builder().id(3).firstName("Julie").lastName("Vigneron").build(); 
+		BusinessEngineer businessEngineer = BusinessEngineer.builder().id(3)
+				.firstName("Julie").lastName("Vigneron").build();
 		Colleague colleague = Colleague.builder().id(1).profileId(1)
 				.phone("6020202020").experience(7)
-				.firstName("modifiedColleague")
-				.lastName("modifiedLastName")
+				.firstName("modifiedColleague").lastName("modifiedLastName")
 				.businessEngineer(businessEngineer)
 				.email("modifiedemail@novediagroup.com")
-				.employmentDate(new Date())
-				.build();
-		
+				.employmentDate(new Date()).build();
+
 		int updateCounter = colleagueDao.save(colleague);
 		Assert.assertEquals(1, updateCounter);
 	}
-	
 
 }
