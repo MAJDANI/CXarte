65b541bcdddd162fbbf4badad055e5768fb00c5e
Boris Tiomo <b.tiomofofou@novediagroup.com>
2013-07-22 11:39:35 +0200

    loginView

--- TalentMap-Web7/src/main/java/com/novedia/talentmap/web/TalentMapApplication.java
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/TalentMapApplication.java
@@ -1,13 +1,8 @@
 package com.novedia.talentmap.web;
 
 import com.novedia.talentmap.web.login.LoginScreen;
-import com.vaadin.data.fieldgroup.FieldGroup;
-import com.vaadin.data.util.ObjectProperty;
-import com.vaadin.data.util.PropertysetItem;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.GridLayout;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -20,61 +15,13 @@ public class TalentMapApplication extends UI {
 	private LoginScreen loginScreen;
 	
 
-	private TextField user;
-
-//	private PasswordField password;
-
-	private Button loginButton;
-    
-	private FieldGroup binder;
-    
-    
-	
 	@Override
 	protected void init(VaadinRequest request) {
-		
-		
-
-//	    user = new TextField("User:");
-//	    user.setInputPrompt("Your username");
-//
-//	    password = new PasswordField("Password:");
-//	    password.setInputPrompt("Your password");
-
-
-//	    loginButton = new Button("Login");
-		
-		// Have an item
-		PropertysetItem item = new PropertysetItem();
-		item.addItemProperty("User", new ObjectProperty<String>(""));
-		item.addItemProperty("Password", new ObjectProperty<String>(""));
-		
-		// Have some layout and create the fields
-		GridLayout form = new GridLayout(2,1);
-//		 
-//		TextField userField = new TextField("User");
-//		form.addComponent(userField);
-//		 
-//		PasswordField password = new PasswordField("Password");
-//		form.addComponent(password);
-	    
-//	    binder = new FieldGroup();
-	    FieldGroup binder = new FieldGroup(item);
-	    form.addComponent(binder.buildAndBind("User", "User"));
-	    form.addComponent(binder.buildAndBind("Password", "Password"));
-//	    form.bind(user, );
-//	    form.bind(password, applicationData);
-	   
-	    
-//	    form.addComponent(view);
-		
-//        view.addComponent(user);
-//        view.addComponent(password);
-//        view.addComponent(loginButton);
-//    	
-        
+		view.setSizeFull();
+		view.addComponent(loginScreen.buildLoginView());
+//		view.setComponentAlignment(loginScreen.getLoginPanel(),Alignment.MIDDLE_CENTER);
+	    setContent(view);
 	}
-
 
 	
 	
@@ -88,13 +35,9 @@ public class TalentMapApplication extends UI {
 	}
 
 
-
-
 	public LoginScreen getLoginScreen() {
 		return loginScreen;
 	}
-
-
 
 
 	public void setLoginScreen(LoginScreen loginScreen) {
--- TalentMap-Web7/src/main/java/com/novedia/talentmap/web/login/LoginScreen.java
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/login/LoginScreen.java
@@ -1,14 +1,21 @@
 package com.novedia.talentmap.web.login;
 
 import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.Reindeer;
 
 @SuppressWarnings("serial")
-public class LoginScreen extends HorizontalLayout { 
+public class LoginScreen extends VerticalLayout{ 
+	
+	private Panel loginPanel;
 	
 	private TextField loginField;
 	
@@ -21,13 +28,114 @@ public class LoginScreen extends HorizontalLayout {
 	
 	private FieldGroup loginForm;
 	
-	private GridLayout loginFormLayout;
+	public LoginScreen(){
+		super();
+	}
 	
 	
-	public HorizontalLayout buildLoginView(){
+	public VerticalLayout buildLoginView(){
 		
+		
+		HorizontalLayout header = new HorizontalLayout();
+		header.addComponent(new Label("Welcome"));
+		
+		loginPanel.addComponent(header);
+		loginPanel.setWidth(null);
+		
+		HorizontalLayout content = new HorizontalLayout();
+		
+		
+		GridLayout loginFormLayout = new GridLayout(3,1);
+		loginFormLayout.setSpacing(true);
+		loginField.setCaption("UserName");
+		
+		passwordField.setCaption("Password");
+		
+		sigIn.setStyleName(Reindeer.BUTTON_LINK);
+		logIn.setCaption("Log In");
+		sigIn.setCaption("Sign In");
+		
+		VerticalLayout v = new VerticalLayout();
+		v.addComponent(logIn);
+		v.addComponent(sigIn);
+		
+		loginFormLayout.addComponent(loginField);
+		loginFormLayout.addComponent(passwordField);
+		loginFormLayout.addComponent(v);
+		
+		content.addComponent(loginFormLayout);
+		
+		loginPanel.addComponent(content);
+		
+		addComponent(loginPanel);
+		setComponentAlignment(loginPanel, Alignment.MIDDLE_CENTER);
 		
 		return this;
 	}
+
+
+	public Panel getLoginPanel() {
+		return loginPanel;
+	}
+
+
+	public void setLoginPanel(Panel loginPanel) {
+		this.loginPanel = loginPanel;
+	}
+
+
+	public PasswordField getPasswordField() {
+		return passwordField;
+	}
+
+
+	public void setPasswordField(PasswordField passwordField) {
+		this.passwordField = passwordField;
+	}
+
+
+	public FieldGroup getLoginForm() {
+		return loginForm;
+	}
+
+
+	public void setLoginForm(FieldGroup loginForm) {
+		this.loginForm = loginForm;
+	}
+
+
+	public TextField getLoginField() {
+		return loginField;
+	}
+
+
+	public void setLoginField(TextField loginField) {
+		this.loginField = loginField;
+	}
+
+
+	public Button getLogIn() {
+		return logIn;
+	}
+
+
+	public void setLogIn(Button logIn) {
+		this.logIn = logIn;
+	}
+
+
+	public Button getSigIn() {
+		return sigIn;
+	}
+
+
+	public void setSigIn(Button sigIn) {
+		this.sigIn = sigIn;
+	}
+	
+	
+	
+	
+	
 
 }
