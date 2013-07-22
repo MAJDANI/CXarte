58965a3270e297e07f7ba2a1c27f2f2110b3e0cc
Johann Maquin <j.maquin@novadiagroup.com>
2013-07-22 09:46:32 +0200

    Vaadin 7 nouveau module web

--- TalentMap-Web7/src/main/java/com/novedia/talentmap/web/TalentMapApplication.java
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/TalentMapApplication.java
@@ -1,20 +1,79 @@
 package com.novedia.talentmap.web;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.data.util.ObjectProperty;
+import com.vaadin.data.util.PropertysetItem;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
-
+//@Theme("talentmaptheme")
 @SuppressWarnings("serial")
 public class TalentMapApplication extends UI {
 
+	private TextField user;
+
+//	private PasswordField password;
+
+	private Button loginButton;
+    
+	private FieldGroup binder;
+    
+    
+	
 	@Override
 	protected void init(VaadinRequest request) {
-		VerticalLayout view = new VerticalLayout();
-        view.addComponent(new Label("Hello Vaadin!"));
-        setContent(view);
+		HorizontalLayout view = new HorizontalLayout();
+		
+
+//	    user = new TextField("User:");
+//	    user.setInputPrompt("Your username");
+//
+//	    password = new PasswordField("Password:");
+//	    password.setInputPrompt("Your password");
+
+
+//	    loginButton = new Button("Login");
+		
+		// Have an item
+		PropertysetItem item = new PropertysetItem();
+		item.addItemProperty("User", new ObjectProperty<String>(""));
+		item.addItemProperty("Password", new ObjectProperty<String>(""));
+		
+		// Have some layout and create the fields
+		GridLayout form = new GridLayout(2,1);
+//		 
+//		TextField userField = new TextField("User");
+//		form.addComponent(userField);
+//		 
+//		PasswordField password = new PasswordField("Password");
+//		form.addComponent(password);
+	    
+//	    binder = new FieldGroup();
+	    FieldGroup binder = new FieldGroup(item);
+	    form.addComponent(binder.buildAndBind("User", "User"));
+	    form.addComponent(binder.buildAndBind("Password", "Password"));
+//	    form.bind(user, );
+//	    form.bind(password, applicationData);
+	   
+	    
+//	    form.addComponent(view);
+		
+//        view.addComponent(user);
+//        view.addComponent(password);
+//        view.addComponent(loginButton);
+//    	
+        
+        setContent(form);
 		
 	}
 
 }
+
