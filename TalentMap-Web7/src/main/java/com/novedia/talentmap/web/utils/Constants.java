bbf105771eeac4ef05781b647e50e3fd9955ede3
Johann Maquin <j.maquin@novadiagroup.com>
2013-07-24 12:31:53 +0200

    RegistrationForm

---
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/utils/Constants.java
@@ -0,0 +1,21 @@
+package com.novedia.talentmap.web.utils;
+
+public class Constants {
+	
+	public static final String  lOGIN_FIELD_LABEL ="Login";
+	
+	public static final String  PASSWORD_FIELD_LABEL ="Password";
+	
+	public static final String  lOGIN_BUTTON_LABEL ="Log In";
+	
+	public static final String  SIGN_IN_BUTTON_LABEL ="SIgn In";
+	
+	public static final String  WELCOMME ="Welcome";
+	
+	public static final String ERROR_LOGIN_MSG = "The Login or password was incorrect.";
+	
+	public static final String TITLE = "Log In Talent Map";
+	
+	
+
+}
