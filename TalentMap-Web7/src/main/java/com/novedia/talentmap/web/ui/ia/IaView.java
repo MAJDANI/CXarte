f8b2df67617d10857e421e753f767458551f4100
Johann Maquin <j.maquin@novadiagroup.com>
2013-07-29 10:47:33 +0200

    Gestion des rôles

---
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/ui/ia/IaView.java
@@ -0,0 +1,26 @@
+package com.novedia.talentmap.web.ui.ia;
+
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+public class IaView extends VerticalLayout {
+	
+	
+	
+	
+	public IaView(){
+		super();
+	}
+	
+	
+	public VerticalLayout buildIaContent(){
+		
+		addComponent(new Label("IAAAAAAAAAAAAAA"));
+		return this;
+	}
+	
+	
+	
+
+}
---
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/utils/ComponentsId.java
@@ -0,0 +1,25 @@
+package com.novedia.talentmap.web.utils;
+
+public class ComponentsId {
+	
+	public static final String LOGIN_FIELD_ID = "login";
+	
+	public static final String PASSWORD_ID = "password";
+	
+	public static final String LOGIN_BUTTON_ID = "logInButton";
+	
+	public static final String SIG_IN_BUTTON_ID = "sigIn";
+	
+	public static final String PROFILE_BUTTON_ID = "profileButton";
+	
+	public static final String EAE_BUTTON_ID = "eaeButton";
+	
+	public static final String OLD_PASSWORD_FIELD_ID = "oldPassword";
+	
+	public static final String NEW_PASSWORD_FIELD_ID = "newPassword";
+	
+	public static final String CONFIRM_PASSWORD_FIELD_ID = "confirmPassword";
+	
+	
+
+}
