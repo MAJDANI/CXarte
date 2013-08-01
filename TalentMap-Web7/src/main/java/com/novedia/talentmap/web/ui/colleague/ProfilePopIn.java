1da105781f5bc533ae98ede1ae93d2666a41ca8a
paola bouadjie <p.bouadjie@novediagroup.com>
2013-08-01 16:53:29 +0200

    profil colleague

---
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/ui/colleague/ProfilePopIn.java
@@ -0,0 +1,239 @@
+package com.novedia.talentmap.web.ui.colleague;
+
+import com.novedia.talentmap.web.ui.colleague.missions.MissionColleagueContent;
+import com.novedia.talentmap.web.utils.Constants;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.themes.Reindeer;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+public class ProfilePopIn extends Window implements ClickListener{
+	
+	/**
+	 * Vaadin components
+	 */
+	private Button administrativeDataButton;
+	
+	private Button skillsButton;
+	
+	private Button missionsHistoryButton;
+	
+	private HorizontalLayout hLayout;
+	
+	private Panel panelLeft;
+	
+	private VerticalLayout menuContent;
+	
+	private Panel panelRight;
+	
+	private VerticalLayout panelRightContent;
+	
+	private MissionColleagueContent missionColleagueContent;
+	
+	
+	private GridLayout colleagueDataFormLayout;
+	
+	private ColleagueDataForm colleagueDataForm;
+	
+	/**
+	 * Default constructor
+	 */
+     public ProfilePopIn(){
+    	 super();
+    	 setWidth("1000px");
+    	 setCaption(Constants.PROFILE_POP_IN_TITLE);
+    	 setModal(true);
+     }
+     
+     
+     /**
+      * Build profilePopIn View
+      * @return Window
+      */
+	public Window buildProfilePopIn(){
+		removeAllComponents();
+		hLayout.setSpacing(true);
+		hLayout.removeAllComponents();
+		buildButtons();
+		buildMenu();
+		buildPanelRightContent();
+		hLayout.addComponent(panelRight);
+	    hLayout.setExpandRatio(panelRight, 1.0f);
+		addComponent(hLayout);
+		return this;
+	}
+
+
+	private void buildPanelRightContent() {
+		panelRight.removeAllComponents();
+		panelRightContent.removeAllComponents();
+		panelRightContent.setSpacing(true);
+		panelRightContent.addComponent(missionColleagueContent.buildViewMissionColleagueContent());
+		panelRight.setContent(panelRightContent);
+		panelRight.setWidth("800px");
+	}
+
+
+	private void buildButtons() {
+		
+		administrativeDataButton.setCaption(Constants.PERSONAL_DATA_LABEL);
+		administrativeDataButton.addStyleName(Reindeer.BUTTON_LINK);
+		administrativeDataButton.addClickListener(this);
+		
+		skillsButton.setCaption(Constants.SKILL_BUTTON_LABEL);
+		skillsButton.addStyleName(Reindeer.BUTTON_LINK);
+		skillsButton.addClickListener(this);
+		
+		missionsHistoryButton.setCaption(Constants.HISTORY_MISSION_LABEL);
+		missionsHistoryButton.addStyleName(Reindeer.BUTTON_LINK);
+		missionsHistoryButton.addClickListener(this);
+		
+	}
+
+
+	private void buildMenu() {
+	    hLayout.setSizeFull();
+	    panelLeft.setWidth("200px");
+	    menuContent.setSpacing(true);
+	    menuContent.setMargin(true);
+	    menuContent.addComponent(administrativeDataButton);
+	    menuContent.addComponent(skillsButton);
+	    menuContent.addComponent(missionsHistoryButton);
+	    panelLeft.setContent(menuContent);
+	    hLayout.addComponent(panelLeft);
+	    
+	}
+
+
+	@Override
+	public void buttonClick(ClickEvent event) {
+		panelRight.removeAllComponents();
+		panelRightContent.removeAllComponents();
+		if (event.getButton().equals(administrativeDataButton)) { 
+			panelRightContent.addComponent(new Label(event.getButton().getCaption()));
+		} else if (event.getButton().equals(skillsButton)) {
+			panelRightContent.addComponent(new Label(event.getButton().getCaption()));
+		} else if(event.getButton().equals(missionsHistoryButton)) {
+			panelRightContent.addComponent(missionColleagueContent.buildViewMissionColleagueContent());
+		}
+		panelRight.setContent(panelRightContent);
+	}
+	
+	
+	public Button getAdministrativeDataButton() {
+		return administrativeDataButton;
+	}
+
+
+	public void setAdministrativeDataButton(Button administrativeDataButton) {
+		this.administrativeDataButton = administrativeDataButton;
+	}
+
+
+	public Button getSkillsButton() {
+		return skillsButton;
+	}
+
+
+	public void setSkillsButton(Button skillsButton) {
+		this.skillsButton = skillsButton;
+	}
+
+
+	public Button getMissionsHistoryButton() {
+		return missionsHistoryButton;
+	}
+
+
+	public void setMissionsHistoryButton(Button missionsHistoryButton) {
+		this.missionsHistoryButton = missionsHistoryButton;
+	}
+
+
+	public Panel getPanelLeft() {
+		return panelLeft;
+	}
+
+
+	public void setPanelLeft(Panel panelLeft) {
+		this.panelLeft = panelLeft;
+	}
+
+
+	public VerticalLayout getMenuContent() {
+		return menuContent;
+	}
+
+
+	public void setMenuContent(VerticalLayout menuContent) {
+		this.menuContent = menuContent;
+	}
+
+
+	public HorizontalLayout gethLayout() {
+		return hLayout;
+	}
+
+
+	public void sethLayout(HorizontalLayout hLayout) {
+		this.hLayout = hLayout;
+	}
+
+
+	public Panel getPanelRight() {
+		return panelRight;
+	}
+
+
+	public void setPanelRight(Panel panelRight) {
+		this.panelRight = panelRight;
+	}
+
+	public VerticalLayout getPanelRightContent() {
+		return panelRightContent;
+	}
+
+
+	public void setPanelRightContent(VerticalLayout panelRightContent) {
+		this.panelRightContent = panelRightContent;
+	}
+
+
+	public MissionColleagueContent getMissionColleagueContent() {
+		return missionColleagueContent;
+	}
+
+
+	public void setMissionColleagueContent(
+			MissionColleagueContent missionColleagueContent) {
+		this.missionColleagueContent = missionColleagueContent;
+	}
+	
+
+	public GridLayout getColleagueDataFormLayout() {
+		return colleagueDataFormLayout;
+	}
+
+
+	public void setColleagueDataFormLayout(GridLayout colleagueDataFormLayout) {
+		this.colleagueDataFormLayout = colleagueDataFormLayout;
+	}
+
+
+	public ColleagueDataForm getColleagueDataForm() {
+		return colleagueDataForm;
+	}
+
+
+	public void setColleagueDataForm(ColleagueDataForm colleagueDataForm) {
+		this.colleagueDataForm = colleagueDataForm;
+	}
+
+}
---
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/ui/colleague/missions/MissionContainer.java
@@ -0,0 +1,65 @@
+package com.novedia.talentmap.web.ui.colleague.missions;
+
+import java.util.List;
+
+import com.novedia.talentmap.model.entity.Mission;
+import com.novedia.talentmap.services.IColleagueService;
+import com.vaadin.data.util.BeanItemContainer;
+
+@SuppressWarnings("serial")
+public class MissionContainer extends BeanItemContainer<Mission> {
+
+	/**
+	 * The colleagueService
+	 */
+	private IColleagueService colleagueService;
+
+    /**
+	 * Default constructor
+	 */
+    public MissionContainer() {
+    	super(Mission.class);
+    }
+
+    /**
+     * Calls the CollaboratorService to retrieve all missions in the Data Base
+     * for the collaborator's id given
+     * 
+     * @param collabId
+     * @return
+     */
+    public MissionContainer fillContainer(int collabId) {
+
+		try {
+			    List<Mission> listMission = this.colleagueService.getAllMissions(collabId);
+			    removeAllItems();
+			    if (listMission != null && !listMission.isEmpty()) {
+					for (Mission m : listMission) {
+					    addBean(m);
+					}
+			    }
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		return this;
+    }
+
+    /**
+     * Get the colleagueService
+     * 
+     * @return the colleagueService
+     */
+    public IColleagueService getColleagueService() {
+	return colleagueService;
+    }
+
+    /**
+     * Set the colleagueService 
+     * 
+     * @param collabService colleagueService to set
+     */
+    public void setColleagueService(IColleagueService collabService) {
+	this.colleagueService = collabService;
+    }
+
+}
