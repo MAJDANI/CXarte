dc54c9c7f8ef1655b4ac5e1676b21699aa1f89a7
paola bouadjie <p.bouadjie@novediagroup.com>
2013-08-01 16:58:09 +0200

    label gestion des missions

---
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/utils/MissionFieldLabel.java
@@ -0,0 +1,33 @@
+package com.novedia.talentmap.web.utils;
+
+public class MissionFieldLabel {
+	
+	public static final String INTITULE = "Title";
+	
+	public static final String CLIENT = "Customer";
+	
+	public static final String LIEU = "Place";
+	
+	public static final String DATE_DEBUT = "Start Date";
+	
+	public static final String DATE_FIN = "End date";
+	
+	public static final String COMMENTAIRE = "Comment";
+	
+	public static final String OUTIL1 = "Tool 1";
+	
+	public static final String OUTIL2 = "Tool 2";
+	
+	public static final String OUTIL3 = "Tool 3";
+	
+	public static final String ADD_MISSION_LABEL = "Add Mission";
+	
+	public static final String EDIT_MISSION_LABEL = "Edit Mission";
+	
+	public static final String DELETE_MISSION_LABEL = "Delete Mission";
+	
+	
+	
+	
+
+}
