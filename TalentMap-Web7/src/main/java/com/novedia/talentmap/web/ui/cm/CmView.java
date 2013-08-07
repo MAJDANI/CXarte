85525a9382500a6c70037b0aa47b2b3b104db0fc
Johann Maquin <j.maquin@novadiagroup.com>
2013-08-07 11:50:41 +0200

    CmView

---
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/ui/cm/CmView.java
@@ -0,0 +1,100 @@
+package com.novedia.talentmap.web.ui.cm;
+
+import com.novedia.talentmap.web.ui.search.SearchPopIn;
+import com.novedia.talentmap.web.utils.ComponentsId;
+import com.novedia.talentmap.web.utils.Constants;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+public class CmView extends VerticalLayout  implements ClickListener {
+	
+	
+	private GridLayout gridLayout;
+	
+	private Panel searchPanel;
+	
+	private Button searchButton;
+	
+	private SearchPopIn searchPopIn;
+	
+	
+	/**
+	 * Default constructor
+	 */
+	public CmView(){
+		super();
+	}
+	
+	/**
+	 * Build CM's content view
+	 * @return VerticalLayout
+	 */
+	public VerticalLayout buildCmContent(){
+		removeAllComponents();
+		buildContent();
+		addComponent(gridLayout);
+		return this;
+	}
+	
+	private void buildContent() {
+		
+		gridLayout.removeAllComponents();
+		gridLayout.setSpacing(true);
+		gridLayout.setRows(2);
+		gridLayout.setColumns(2);
+		
+		searchPanel.removeAllComponents();
+		searchButton.setCaption(Constants.SEARCH_LABEL);
+		searchButton.setId(ComponentsId.SEARCH_BUTTON_ID);
+		searchButton.addClickListener(this);
+		searchPanel.addComponent(searchButton);
+		
+		gridLayout.addComponent(searchPanel);
+	}
+	
+	@Override
+	public void buttonClick(ClickEvent event) {
+		if(event.getButton().equals(searchButton)){
+			getUI().addWindow(searchPopIn.buildSearchPopIn());
+		}
+		
+	}
+
+	public GridLayout getGridLayout() {
+		return gridLayout;
+	}
+
+	public void setGridLayout(GridLayout gridLayout) {
+		this.gridLayout = gridLayout;
+	}
+
+	public Panel getSearchPanel() {
+		return searchPanel;
+	}
+
+	public void setSearchPanel(Panel searchPanel) {
+		this.searchPanel = searchPanel;
+	}
+
+	public Button getSearchButton() {
+		return searchButton;
+	}
+
+	public void setSearchButton(Button searchButton) {
+		this.searchButton = searchButton;
+	}
+
+	public SearchPopIn getSearchPopIn() {
+		return searchPopIn;
+	}
+
+	public void setSearchPopIn(SearchPopIn searchPopIn) {
+		this.searchPopIn = searchPopIn;
+	}
+
+}
