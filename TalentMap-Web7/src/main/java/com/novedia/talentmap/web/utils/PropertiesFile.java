386fc348496efd0e80a96cd1cac3b019d8bd8835
Boris Tiomo <b.tiomofofou@novediagroup.com>
2013-09-03 15:38:54 +0200

    fichier des constantes de properties

---
+++ TalentMap-Web7/src/main/java/com/novedia/talentmap/web/utils/PropertiesFile.java
@@ -0,0 +1,11 @@
+package com.novedia.talentmap.web.utils;
+
+public class PropertiesFile {
+	
+	public static final String LOGIN_SCREEN_PROPERTIES = "com.novedia.talentmap.web.properties.login.LoginScreen";
+	
+	public static final String REGISTRATION_FORM_PROPERTIES = "com.novedia.talentmap.web.properties.resgistration.RegistrationForm";
+	
+	
+
+}
